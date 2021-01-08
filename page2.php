226e538501e9804d9eb5b261b653f588d2d7d610
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2021-01-08 11:57:13 +0100

    oui

--- page2.php
+++ page2.php
@@ -3,18 +3,18 @@
  <h5 class="ml-4 size-text ">Bonjour <?= $firstname ?>  <?= $name ?></h5>
  <p class="ml-4 size-text">Recapitulatif de votre formulaire...</p>
  <ul>
-        <li>Date de Naissance : <?= $Birthday;?></li>
-        <li>Votre Pays de Naissance : <?=$country;?></li>
-        <li>Vous etes : <?= $nation;?></li>
-        <li>Votre Adresse mail est : <?= $mail;?></li>
-        <li>Votre numero de téléphone est le : <?= $phone;?></li>
-        <li>Votre diplome : <?= empty($diplome) ? "vous n'avez rien mis" : $diplome; ?></li>
-        <li>Votre numero Pole Emploi est : <?=$poleEmploiNumber;?></li>
-        <li>Votre Nombre de badge est de : <?= empty($badgeNumber)? "0" : $badgeNumber; ?></li>
-        <li>Votre lien Code Academy est :<a href="<?= $linkPass;?>"> Ton lien </a> </li>
-        <li>Pour la question "Si vous étiez un super hero vous seriez qui et pourquoi ?", vous avez repondu : <?= $_POST['superHero'];?></li>
-        <li>Pour "Racontez-nous un de vos 'hacks' (pas forcément technique ou informatique)", vous avez repondu : <?= $_POST['hack'];?></li>
-        <li>Pour la question "Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?", vous avez repondu : <?= $_POST['exp'];?></li>
+        <li>Date de Naissance :<?=$Birthday ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre Pays de Naissance : <?=$country ?? 'Vous n\'avez rien mis'?></li>
+        <li>Vous etes : <?= $nation ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre Adresse mail est : <?= $mail ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre numero de téléphone est le : <?= $phone ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre diplome : <?= $diplome ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre numero Pole Emploi est :<?= $poleEmploiNumber ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre Nombre de badge est de : <?= $badgeNumber ?? 'Vous n\'avez rien mis'?></li>
+        <li>Votre lien Code Academy est :<a href="<?= $linkPass ?? 'Vous n\'avez rien mis'?>"> Ton lien </a> </li>
+        <li>Pour la question "Si vous étiez un super hero vous seriez qui et pourquoi ?", vous avez repondu :<?= $superHero ?? 'Vous n\'avez rien mis'?></li>
+        <li>Pour "Racontez-nous un de vos 'hacks' (pas forcément technique ou informatique)", vous avez repondu :<?= $hack ?? 'Vous n\'avez rien mis'?></li>
+        <li>Pour la question "Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?", vous avez repondu :  <?= $exp ?? 'Vous n\'avez rien mis'?></li>
  </ul>
  <img class="img-fluid ml-5 mt-5" src="assets\img\source.gif" alt="pulp fiction">
 </div>
